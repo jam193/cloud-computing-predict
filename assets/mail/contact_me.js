cc01237b208ac52d6a24a64d62061fc4c5486dab
jam193 <85170166+jam193@users.noreply.github.com>
2023-11-02 13:05:14 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://tb9gaume5l.execute-api.eu-west-2.amazonaws.com/default/lambda_naza",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
